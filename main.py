0c3e1aed747a5a24589d0aac11b362c859577a7a
RubenKelevra <cyrond@gmail.com>
2014-01-21 03:07:23 +0100

    typo

--- main.py
+++ main.py
@@ -206,9 +206,9 @@ conversation_dict2[51] = "Dead Like Me"
 
 remove_strings = ["dxvid", "xvid", "staffel", "episode", "the", "german", "ger", "intro", "ep", "avi", "divx", "flv", "ogm", "ac3", "0W4", "x264", "p0w4", "Prim3time", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "1080p", "720p"]
 seperators = ["_", " ", ".", "-", ","]
-Try:
+try:
 	os.chdir(download_dir)
-Except FileNotFoundError:
+except FileNotFoundError:
 	print("Error: Download-Directory not found")
 	exit(1)
 
