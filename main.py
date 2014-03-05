478ea3a7542f5a911c53d73749ca85eeb84a36cb
RubenKelevra <cyrond@gmail.com>
2014-03-05 02:00:53 +0100

    adding "Drachenreiter von Berk, Die"

--- main.py
+++ main.py
@@ -304,6 +304,10 @@ conversation_dict3[61] = "film"
 conversation_dict["exodus"] = 62
 conversation_dict2[62] = "Exodus Erde"
 conversation_dict3[62] = "film"
+conversation_dict["berk"] = 63
+conversation_dict["drachenreiter"] = 63
+conversation_dict2[63] = "Drachenreiter von Berk, Die"
+conversation_dict3[63] = "animation"
 
 remove_strings = ["dxvid", "xvid", "p0w4", "Prim3time", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "1080p", "720p"]
 seperators = ["_", " ", ".", "-", ","]
