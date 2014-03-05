7fdce80dae0c23308ba6c183461c986801c91b68
RubenKelevra <cyrond@gmail.com>
2014-03-05 01:33:08 +0100

    adding "Person of Interest"

--- main.py
+++ main.py
@@ -287,6 +287,10 @@ conversation_dict3[57] = "film"
 conversation_dict["sleepyhollow"] = 58
 conversation_dict2[58] = "Sleepy Hollow"
 conversation_dict3[58] = "film"
+conversation_dict["person"] = 59
+conversation_dict["interest"] = 59
+conversation_dict2[59] = "Person of Interest"
+conversation_dict3[59] = "film"
 
 remove_strings = ["dxvid", "xvid", "p0w4", "Prim3time", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "1080p", "720p"]
 seperators = ["_", " ", ".", "-", ","]
