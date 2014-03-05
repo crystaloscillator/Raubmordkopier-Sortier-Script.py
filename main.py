ebb1ab39c02cc8fed4630a67eea222b0c278c095
RubenKelevra <cyrond@gmail.com>
2014-03-05 01:35:26 +0100

    adding "Hell on Wheels"

--- main.py
+++ main.py
@@ -294,6 +294,11 @@ conversation_dict3[59] = "film"
 conversation_dict["firefly"] = 60
 conversation_dict2[60] = "Firefly"
 conversation_dict3[60] = "film"
+conversation_dict["wheels"] = 61
+conversation_dict["hell"] = 61
+conversation_dict["hellonwheels"] = 61
+conversation_dict2[61] = "Hell on Wheels"
+conversation_dict3[61] = "film"
 
 remove_strings = ["dxvid", "xvid", "p0w4", "Prim3time", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "1080p", "720p"]
 seperators = ["_", " ", ".", "-", ","]
