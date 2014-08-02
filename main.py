a6efa410dd87e001a32778b69585561e7de25dcf
@RubenKelevra <cyrond@gmail.com>
2014-08-02 03:46:32 +0200

    add Sailor Moon

--- main.py
+++ main.py
@@ -344,6 +344,9 @@ conversation_dict3[71] = "film"
 conversation_dict["lie"] = 72
 conversation_dict2[72] = "Lie to Me"
 conversation_dict3[72] = "film"
+conversation_dict["sailor"] = 73
+conversation_dict2[73] = "Sailor Moon"
+conversation_dict3[73] = "animation"
 
 
 remove_strings = ["dxvid", "xvid", "staffel", "episode", "the", "german", "ger", "intro", "ep", "avi", "divx", "flv", "ogm", "ac3", "0W4", "x264", "X264", "p0w4", "Prim3time", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "1080p", "720p"]
