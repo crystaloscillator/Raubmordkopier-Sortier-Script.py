7bb73467a2641d63048445bcfea06a34665f39a8
@RubenKelevra <cyrond@gmail.com>
2014-08-02 04:00:39 +0200

    add Peaky Blinders - Gangs of Birmingham

--- main.py
+++ main.py
@@ -365,6 +365,11 @@ conversation_dict["juw"] = 77
 conversation_dict["blackadder"] = 77
 conversation_dict2[77] = "Blackadder"
 conversation_dict3[77] = "film"
+conversation_dict["birmingham"] = 78
+conversation_dict["blinders"] = 78
+conversation_dict["peaky"] = 78
+conversation_dict2[78] = "Peaky Blinders - Gangs of Birmingham"
+conversation_dict3[78] = "film"
 
 
 remove_strings = ["dxvid", "xvid", "staffel", "episode", "the", "german", "ger", "intro", "ep", "avi", "divx", "flv", "ogm", "ac3", "0W4", "x264", "X264", "p0w4", "Prim3time", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "1080p", "720p"]
