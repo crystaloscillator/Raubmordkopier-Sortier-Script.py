811cc1ed5de60c1bb2ac68bb03b607ba7b1562c9
@RubenKelevra <cyrond@gmail.com>
2014-08-02 03:39:06 +0200

    fix copy'n'paste-error

--- main.py
+++ main.py
@@ -326,8 +326,8 @@ conversation_dict3[66] = "film"
 conversation_dict["abenobashi"] = 67
 conversation_dict["mahou"] = 67
 conversation_dict["shoutengai"] = 67
-conversation_dict2[66] = "Abenobashi Mahou Shoutengai"
-conversation_dict3[66] = "animation"
+conversation_dict2[67] = "Abenobashi Mahou Shoutengai"
+conversation_dict3[67] = "animation"
 
 remove_strings = ["dxvid", "xvid", "staffel", "episode", "the", "german", "ger", "intro", "ep", "avi", "divx", "flv", "ogm", "ac3", "0W4", "x264", "X264", "p0w4", "Prim3time", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "1080p", "720p"]
 seperators = ["_", " ", ".", "-", ",", "(", ")", "[", "]"]
