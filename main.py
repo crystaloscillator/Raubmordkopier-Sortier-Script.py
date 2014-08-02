e543829e0f7108cef982470333fa104c782ad539
@RubenKelevra <cyrond@gmail.com>
2014-08-02 04:16:54 +0200

    add Jeeves and Wooster

--- main.py
+++ main.py
@@ -363,8 +363,8 @@ conversation_dict3[75] = "film"
 conversation_dict["americans"] = 76
 conversation_dict2[76] = "Americans, The"
 conversation_dict3[76] = "film"
-conversation_dict["juw"] = 77
 conversation_dict["blackadder"] = 77
+conversation_dict["adder"] = 77
 conversation_dict2[77] = "Blackadder"
 conversation_dict3[77] = "film"
 conversation_dict["birmingham"] = 78
@@ -376,7 +376,11 @@ conversation_dict3[78] = "film"
 conversation_dict["pinky"] = 79
 conversation_dict2[79] = "Pinky und der Brain"
 conversation_dict3[79] = "film"
-
+conversation_dict["juw"] = 80
+conversation_dict["jeeves"] = 80
+conversation_dict["wooster"] = 80
+conversation_dict2[80] = "Jeeves and Wooster"
+conversation_dict3[80] = "film"
 
 remove_strings = ["dxvid", "xvid", "staffel", "episode", "the", "german", "ger", "intro", "ep", "avi", "divx", "flv", "ogm", "ac3", "0W4", "x264", "X264", "p0w4", "Prim3time", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "1080p", "720p"]
 seperators = ["_", " ", ".", "-", ",", "(", ")", "[", "]"]
