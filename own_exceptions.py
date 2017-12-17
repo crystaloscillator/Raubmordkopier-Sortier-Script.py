21f78bcffea6f5488d35194cc76105aaeb134ddf
RubenKelevra <ruben@freifunk-nrw.de>
2017-12-17 20:02:26 +0100

    do stuff

---
+++ own_exceptions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+class ResultNotUnique(Exception):
+	pass
+
+
+class DataValidationError(Exception):
+	pass
+
+
+class TooManyNumbers(Exception):
+	pass
+
+
+class TooLessNumbers(Exception):
+	pass
